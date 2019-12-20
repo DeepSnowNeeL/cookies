51092c09acc13ddee23417f51dfa97df170e31c4
Cruz Maximilien <maximilien.cruz@gmail.com>
2019-12-20 02:14:15 +0100

    update to angular 9

--- src/server/server-cookies.module.ts
+++ src/server/server-cookies.module.ts
@@ -1,19 +1,24 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from "@angular/core";
 
-import { CookiesOptions } from '../cookies-options';
-import { COOKIES_OPTIONS, CookiesOptionsService } from '../cookies-options.service';
-import { CookiesService } from '../cookies.service';
-import { ServerCookiesService } from './server-cookies.service';
+import { CookiesOptions } from "../cookies-options";
+import {
+  COOKIES_OPTIONS,
+  CookiesOptionsService
+} from "../cookies-options.service";
+import { CookiesService } from "../cookies.service";
+import { ServerCookiesService } from "./server-cookies.service";
 
 @NgModule()
 export class ServerCookiesModule {
-  static forRoot(options: CookiesOptions = {}): ModuleWithProviders {
+  static forRoot(
+    options: CookiesOptions = {}
+  ): ModuleWithProviders<ServerCookiesModule> {
     return {
       ngModule: ServerCookiesModule,
       providers: [
-        {provide: COOKIES_OPTIONS, useValue: options},
+        { provide: COOKIES_OPTIONS, useValue: options },
         CookiesOptionsService,
-        {provide: CookiesService, useClass: ServerCookiesService}
+        { provide: CookiesService, useClass: ServerCookiesService }
       ]
     };
   }
